b43df456798fc7667e13ee87a14e89b816c0de7d
Samuel Burer <samuel-burer@uiowa.edu>
2011-10-18 15:02:30 -0500

    Added Win32 binaries. Actually not tested because I don't have CPLEX under Win32.

--- opt_dnn_compile.m
+++ opt_dnn_compile.m
@@ -1,12 +1,8 @@
-%mex -largeArrayDims -O opt_dnn_subprob_Y.c
-%mex -largeArrayDims -O opt_dnn_subprob_Z.c -lmwlapack -lmwblas
 mex -largeArrayDims opt_dnn_subprob_Y.c -lmwlapack -lmwblas
 mex -largeArrayDims opt_dnn_subprob_Z.c -lmwlapack -lmwblas
 
-%% Possible Windows variation
-%% In Windows, do not append underscores to blas function names in .c files.
+%% Possible Windows variation: uncomment the following commands
+%% Also, do not append underscores to blas function names in .c files.
 
-%% mex -O project.c C:\Progra~1\MATLAB\R2008a\extern\lib\win32\lcc\libmwlapack.lib C:\Progra~1\MATLAB\R2008a\extern\lib\win32\lcc\libmwblas.lib 
-
-
-% mex -largeArrayDims -I/user/biz/jieqchen/matlab/cplex110/include/ilcplex -L/user/biz/jieqchen/matlab/cplex110/lib/x86-64_sles9.0_3.3/static_pic cplexint.c -lcplex
+% mex -O opt_dnn_subprob_Y.c C:\Progra~1\MATLAB\R2009a\extern\lib\win32\lcc\libmwlapack.lib C:\Progra~1\MATLAB\R2009a\extern\lib\win32\lcc\libmwblas.lib 
+% mex -O opt_dnn_subprob_Z.c C:\Progra~1\MATLAB\R2009a\extern\lib\win32\lcc\libmwlapack.lib C:\Progra~1\MATLAB\R2009a\extern\lib\win32\lcc\libmwblas.lib 
