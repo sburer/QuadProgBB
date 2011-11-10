ac4021763489c3628ca3f9416bbafd866724f4b7
Samuel Burer <samuel-burer@uiowa.edu>
2011-11-10 06:41:56 -0600

    Alter test.m (save workspace).

--- tests/user_20111108/test.m
+++ tests/user_20111108/test.m
@@ -10,5 +10,6 @@ load x_U
 opsiyon=struct('max_time',360000000);
 opsiyon.use_single_processor = 1;
 opsiyon.verbosity = 3;
-[x,fValCheck,ExitFlag,Output,Lambda] = quadprog  (F,c,[],[],Aeq(1:end-1,:),beq(1:end-1),x_L,x_U,x_0);
+% [x,fValCheck,ExitFlag,Output,Lambda] = quadprog  (F,c,[],[],Aeq(1:end-1,:),beq(1:end-1),x_L,x_U,x_0);
 [x,fValCheck,time,status]            = quadprogbb(F,c,[],[],Aeq(1:end-1,:),beq(1:end-1),x_L,x_U,opsiyon);
+save workspace
